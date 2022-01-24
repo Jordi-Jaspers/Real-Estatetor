f9d74e1d76f71cceadc383b0929dcd7f5ed0c22c
jordi-jaspers <34105935+Jordi-Jaspers@users.noreply.github.com>
2022-01-24 16:19:16 +0100

    basic react application setup

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div>
+        <h1 className="text-3xl font-bold underline">
+            This is a navbar.
+        </h1>
+    </div>
+  );
+};
+
+export default Navbar;
